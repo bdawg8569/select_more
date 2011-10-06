fdccaa9ce6a158700e78cbce9298eed50711d9fa
Brandonian <brandon@brandonmorrison.com>
2011-10-06 16:00:19 -0400

    Initial stab at js for hiding/showing textfields

--- select_more.js
+++ select_more.js
@@ -1,6 +1,16 @@
-// JavaScript Document
-jQuery(document).ready(function() {
-  jQuery('div.form-type-select-more').each(function() {
-		alert('tester');							   
-  });
-});
+(function ($) {
+  Drupal.behaviors.selectMore = {
+    attach: function (context, settings) {
+      var mySettings = settings.selectMore;
+      for (i in mySettings) {
+        if ($('.form-item-' + mySettings[i] + ' input.form-checkbox:checked').val() == undefined) {
+          $('.form-item-' + mySettings[i] + ' .form-type-textarea').hide();
+        }
+        
+        $('.form-item-' + mySettings[i] + ' input.form-checkbox').change(function() {
+          $(this).parent().parent().find('.form-type-textarea').slideToggle('fast');
+        });
+      }
+    }
+  }
+})(jQuery);
