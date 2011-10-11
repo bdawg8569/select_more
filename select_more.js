82a55b4bf7e5e7f23a08fb295db7c5f2e2150698
Brian Williams <brian@djcase.com>
2011-10-11 12:41:15 -0400

    added some jquery to show textareas when an error occurs do to a value in the tetextarea but the chexbox is off.

--- select_more.js
+++ select_more.js
@@ -2,13 +2,19 @@
   Drupal.behaviors.selectMore = {
     attach: function (context, settings) {
       var mySettings = settings.selectMore;
-      for (i in mySettings) {
+      for (i in mySettings) { 
+        
         if ($('.form-item-' + mySettings[i] + ' input.form-checkbox:checked').val() == undefined) {
           $('.form-item-' + mySettings[i] + ' .form-type-textarea').hide();
         }
         
         $('.form-item-' + mySettings[i] + ' input.form-checkbox').change(function() {
           $(this).parent().parent().find('.form-type-textarea').slideToggle('fast');
+        });
+        
+        //if there is an error on the form, show the textarea so its clear where the problem is
+        $('.form-item-' + mySettings[i] + ' input.form-checkbox.error').each(function() {
+          $(this).parent().parent().find('.form-type-textarea').show();
         });
       }
     }
